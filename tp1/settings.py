a3102ceb76a6d1b072dc10e927cb445470bd96bf
Mateo Bossio <mateofbossio@gmail.com>
2020-04-07 12:33:29 -0300

    agregue ip para q puedan entrar desde otro dispo en la red

--- tp1/settings.py
+++ tp1/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '9-99)d-+#=t*jz*^gob=e*)m2f_yeo&dbsw4v(o#bl%8=jm66c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "192.168.0.26",
+]
 
 
 # Application definition
